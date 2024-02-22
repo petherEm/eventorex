12ee6161352e8b9f091f314dfe810eecc04bcbc4
Piotr Maciejewski <pether.maciejewski@gmail.com>
2024-02-22 10:40:39 +0100

    progress 2.1

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -6,6 +6,7 @@ import { SearchParamProps } from "@/types";
 import Image from "next/image";
 import { formatDateTime } from "@/lib/utils";
 import Collection from "@/components/shared/Collection";
+import CheckoutButton from "@/components/shared/CheckoutButton";
 
 const EventDetails = async ({
   params: { id },
@@ -52,6 +53,7 @@ const EventDetails = async ({
               </div>
             </div>
             {/* CHECKOUT BUTTON */}
+            <CheckoutButton event={event} />
 
             <div className="flex flex-col gap-5">
               <div className="flex gap-2 md:gap-3">
